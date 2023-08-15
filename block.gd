d1605c08908e80a7d772be4e2bfe3f045abfe53c
Michal Cenek <michal.cenek.1@gmail.com>
2023-08-15 20:13:34 +0200

    changed spaces to tabs :-------)

--- block.gd
+++ block.gd
@@ -10,26 +10,26 @@ signal clicked(sender, block_position: Vector2)
 var dsum = 0.0
 
 func _ready():
-  if not interactable:
-    var sb = $StaticBody2D
-    sb.collision_layer = 0
-    sb.collision_mask = 0
+	if not interactable:
+		var sb = $StaticBody2D
+		sb.collision_layer = 0
+		sb.collision_mask = 0
 
-  interactable = false
+	interactable = false
 
-  if not breakable:
-    $Body.color = Color('#362c38')
+	if not breakable:
+		$Body.color = Color('#362c38')
 
 func _process(delta):
-  dsum += delta
+	dsum += delta
 
-  if dsum > 0.3:
-    interactable = true
-    set_process(false)
+	if dsum > 0.3:
+		interactable = true
+		set_process(false)
 
 func _on_static_body_2d_input_event(_viewport, event, _shape_idx):
-  if not interactable:
-    return
+	if not interactable:
+		return
 
-  if event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
-    clicked.emit(self, position)
+	if event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
+		clicked.emit(self, position)
--- world.gd
+++ world.gd
@@ -4,13 +4,13 @@ var grid_size: int
 var block_size: int
 
 func _init(p_grid_size: int, p_block_size: int):
-  grid_size = p_grid_size
-  block_size = p_block_size
+	grid_size = p_grid_size
+	block_size = p_block_size
 
 func world_to_grid(v: Vector2) -> Vector2:
-  var result = v / block_size
-  result = result.floor()
-  return result
+	var result = v / block_size
+	result = result.floor()
+	return result
 
 func grid_to_world(v: Vector2) -> Vector2:
-  return v * block_size
+	return v * block_size
