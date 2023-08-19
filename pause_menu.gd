58d2395952c0c1df61686e90ee6a9403dcc191f1
Michal Cenek <michal.cenek.1@gmail.com>
2023-08-19 18:01:17 +0200

    added main menu button to pause menu

--- pause_menu.gd
+++ pause_menu.gd
@@ -14,6 +14,10 @@ func unpause():
 	dsum = 0.0
 	get_tree().paused = false
 
+func to_menu():
+	get_tree().change_scene_to_file("res://main_menu.tscn")
+
+
 func _process(delta):
 	if can_unpause and Input.is_action_just_pressed('ui_cancel'):
 		unpause()
@@ -22,8 +26,14 @@ func _process(delta):
 	if dsum > 0.1:
 		can_unpause = true
 
+
 func _on_resume_button_down():
 	unpause()
 
 func _on_exit_button_down():
 	quit_game()
+
+func _on_menu_button_down():
+	get_tree().paused = false
+	to_menu()
+
